9d1427184f0c8efe3921a62fb8d6e9f9ea5a4c63
Kshitijagadekar <kshitijatajane.11@gmail.com>
2025-04-29 14:49:33 +0530

    new commit for package

---
+++ PackageProject/src/com/tka/controller/TestA.java
@@ -0,0 +1,19 @@
+package com.tka.controller;
+
+public class TestA {
+
+	public static void main(String[] args) {
+		A var = new A();
+		
+		//System.out.println(var.p);
+		
+		//var.p = 30;
+		
+		//System.out.println(var.p);
+		
+		var.m1();
+		
+
+	}
+
+}
---
+++ PackageProject/src/com/tka/test/MyTest.java
@@ -0,0 +1,14 @@
+package com.tka.test;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+
+		com.tka.controller.A abc = new com.tka.controller.A();
+		abc.m1();
+
+		com.tka.test.A abc1 = new com.tka.test.A();
+		abc1.m1();
+	}
+
+}
